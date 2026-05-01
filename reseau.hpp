0ca759e78bdf743aa64d494c5f0e7cf4d4b3b327
bakame03 <nganjialdoalex@gmail.com>
2026-05-01 18:22:25 +0200

    feat: add INFINI constant and declare lireTemps function for travel time processing

--- reseau.hpp
+++ reseau.hpp
@@ -14,4 +14,12 @@ struct Ville {
 // ca prend juste le nom du fichier en parametre et retourne un tableau de type structure ville
 std::vector<Ville> lireVilles(const std::string& nomFichier);
 
+// -------------------------------------------------------------------------------------------------------
+// ca c'est pour la constante pour représenter l'infini donc when pas de route directe
+const int INFINI = 999999;
+
+// ca c'est pour juste lire le fichier temps.csv et retourner une matrice 2D
+// la tailleMax va etre le nombre total de ville
+std::vector<std::vector<int>> lireTemps(const std::string& nomFichier, size_t tailleMax);
+
 #endif

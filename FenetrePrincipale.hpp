68ffba6bce48cfbbbc61a3753493b1a92f39efbf
bakame03 <nganjialdoalex@gmail.com>
2026-05-02 06:28:41 +0200

    feat: add UI components and layout to FenetrePrincipale for city path calculation

--- FenetrePrincipale.hpp
+++ FenetrePrincipale.hpp
@@ -1,17 +1,22 @@
 #ifndef __FENETREPRINCIPALE__
 #define __FENETREPRINCIPALE__
 
-// on inclut les trucs de base de Qt dont on va avoir besoin
+// ici on inclut les trucs de base de Qt dont on va avoir besoin
 #include <QMainWindow>
 #include <QPushButton>
+#include <QComboBox>
+#include <QLabel>
+#include <QVBoxLayout>
+#include <QWidget>
 
-// on inclut notre back-end coz on en aura besoin plus tard pour lier l'interface aux calculs
+// on inclut notre back-end pour lier l'interface aux calculs
 #include "reseau.hpp"
 
 // ca c'est notre fenetre principale, elle herite de QMainWindow
 class FenetrePrincipale : public QMainWindow {
     // so here we put the magic word pour que la fenetre puisse comprendre les clics de souris
     Q_OBJECT
+
     public:
         // le constructeur, il prend un parent nul par defaut coz c'est la fenetre de base
         FenetrePrincipale(QWidget *parent = nullptr);
@@ -19,11 +24,16 @@ class FenetrePrincipale : public QMainWindow {
         ~FenetrePrincipale();
 
     private:
-        // pour l'instant on declare juste un petit bouton pour tester
-        // c'est un pointeur coz Qt prefere gerer la memoire dynamiquement
-        // ici ca vas nous permettre when we make a new de ne pas faire le delete le QWidget parent le fera on it's own
-        // ce qui vas faire qu'il vas faire aussi les delete de ses enfants     
-        QPushButton *boutonTest;
+        QComboBox *comboDepart;
+        QComboBox *comboArrivee;
+        QPushButton *boutonCalculer;
+        QLabel *labelResultat;
+        
+        // Le layout qui va ranger tout ca de haut en bas
+        QVBoxLayout *layoutPrincipal;
+        
+        // Le widget central qui va servir de conteneur global pour le layout
+        QWidget *widgetCentral;
 };
 
 #endif
